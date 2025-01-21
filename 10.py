5d566d0cb5b706f4a52b89f14d83d625711117a4
Ankit105114 <ankitverma105114@gmail.com>
2025-01-21 11:55:36 +0530

    first commit

---
+++ 10.py
@@ -0,0 +1,12 @@
+# principal = float(input("Enter the principal amount: "))
+# rate = float(input("Enter the rate of interest: "))
+# time = float(input("Enter the time in years: "))
+# compound_interest = principal * (1 + rate / 100) ** time - principal
+# print(f"The compound interest is: {compound_interest}")
+
+#simple interest 
+p=float(input("Enter the principal"))
+r=float(input("Enter the rate"))
+t=float(input("Enter the time"))
+si=(p*r*t)/100
+print("Simple Interest is:",si)
